f285782a813883a7aef9576ecfa9606aea0aba60
Cesar Lizurey <cesar@lizurey.fr>
2026-04-24 19:45:56 +0200

    Fix: ajouter helper au bundle et corriger les chemins postinstall (/usr/lib/PartaGPU/)

--- src-tauri/scripts/postinstall.sh
+++ src-tauri/scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /usr/local/lib/partagpu
-cp /usr/lib/partagpu/resources/partagpu-helper /usr/local/lib/partagpu/partagpu-helper
+cp /usr/lib/PartaGPU/resources/partagpu-helper /usr/local/lib/partagpu/partagpu-helper
 chmod 755 /usr/local/lib/partagpu/partagpu-helper
 chown root:root /usr/local/lib/partagpu/partagpu-helper
-cp /usr/lib/partagpu/resources/com.partagpu.policy /usr/share/polkit-1/actions/com.partagpu.policy
+cp /usr/lib/PartaGPU/resources/com.partagpu.policy /usr/share/polkit-1/actions/com.partagpu.policy
 chmod 644 /usr/share/polkit-1/actions/com.partagpu.policy
